23dbac9bbfa316b9b860c77e60269b897243f15d
Alexandr <kadavar32@yahoo.com>
2015-09-17 00:28:36 +0300

    Home page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,13 +3,20 @@ require 'spec_helper'
 require 'support/utilities'
 
 describe "Static pages" do
+    subject {page}
     
-    describe "Home page" do
-
-        it "should have the content 'Lunch Ordering'" do
-    visit '/static_pages/home'
-    expect(page).to have_content('Lunch Ordering')
-  end
-end
+    shared_examples_for "all static pages" do
+        it {should have_selector('h1',text:heading) }
+        it {should have_title (full_title(page_title))}
+    end
+    
+    
+     describe "Home page" do
+      before {visit root_path}
+         let(:heading) {'Lunch Ordering'}
+         let(:page_title) {''}
+         it {should_not have_title ('Lunch Ordering')}
+         it { should have_link("Sign up now", href: root_path) }
+     end
 
 end
