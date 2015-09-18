1940e9f9772f94d14d589b363fec19ea5c73dd0f
Alexandr <kadavar32@yahoo.com>
2015-09-19 00:14:52 +0300

    add auto

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-    
+   before_action :signed_in_user, only: [:edit, :update] 
+      before_action :correct_user,   only: [:edit, :update]
     def show
         @user=User.find(params[:id])
     end
@@ -23,7 +24,6 @@ class UsersController < ApplicationController
   end
     
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -36,7 +36,18 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+                            :password_confirmation)
     end
-   
+    
+  def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
