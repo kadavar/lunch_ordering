a5e7d51326a3ec6bf9d6d3eea9d9c98824297bc8
Alexandr <kadavar32@yahoo.com>
2015-09-25 15:32:29 +0300

    create_menus_Fix

--- db/migrate/20150916231050_add_index.rb
+++ db/migrate/20150916231050_add_index.rb
@@ -1,8 +1,8 @@
 class AddIndex < ActiveRecord::Migration
   def change
-      add_index :users_roles, :user_id
-      add_index :users_roles, :role_id
-      add_index :users_roles, [:user_id, :role_id], unique: true
+      add_index :user_roles, :user_id
+      add_index :user_roles, :role_id
+      add_index :user_roles, [:user_id, :role_id], unique: true
       add_index "users", ["email"], name: "index_users_on_email", unique: true
   end
 end
