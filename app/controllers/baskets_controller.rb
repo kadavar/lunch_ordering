c73b607007bb0a9b5bfde6de8397b38b675b87e5
Alexandr <kadavar32@yahoo.com>
2015-10-01 02:06:50 +0300

    refactoring basket controller

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -2,18 +2,13 @@ class BasketsController < ApplicationController
 
 
   def create
-      already_exist=Basket.find_by(user_id: current_user.id, food_course: Food.find(params[:food]).course)
+    already_exist=Basket.find_by(user_id: current_user.id, food_course: Food.find(params[:food]).course)
     if (already_exist)
       already_exist.destroy
-
     end
     Basket.create(food_id: params[:food], user_id: current_user.id, food_course: Food.find(params[:food]).course)
-
     respond_to do |format|
-
-
       format.js
-
     end
   end
 
