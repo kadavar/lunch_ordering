10a142c47b7daf0682bd1e22b3e072c76583b86b
Alexandr <kadavar32@yahoo.com>
2015-10-03 06:23:33 +0300

    specs create/destroy baskets with ajax

--- spec/controllers/baskets_controller_spec.rb
+++ spec/controllers/baskets_controller_spec.rb
@@ -17,6 +17,9 @@ RSpec.describe  BasketsController,  :type => :controller do
                  expect do
                       xhr :post, :create, :food=> @food.id
                  end.to change(Basket, :count).by(1)
+                 expect do
+                      xhr :post, :create, :food=> @food.id
+                 end.not_to change(Basket, :count)
 
             end
           it "destroy basket" do
