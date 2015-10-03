d2b7b336837d0a85959a65dd09ced29d73e1befb
Alexandr <kadavar32@yahoo.com>
2015-10-03 06:42:36 +0300

    formate code

--- spec/controllers/baskets_controller_spec.rb
+++ spec/controllers/baskets_controller_spec.rb
@@ -3,35 +3,35 @@ require 'spec_helper'
 
 
 #RSpec.configure {|c| c.include SessionsHelper }
-RSpec.describe  BasketsController,  :type => :controller do
+RSpec.describe BasketsController, :type => :controller do
 
   describe "basket" do
 
-       User.delete_all
+    User.delete_all
 
-            it "should increment the basket count" do
+    it "should increment the basket count" do
 
-                 @user=FactoryGirl.create(:user)
-                 @food=FactoryGirl.create(:food)
-                 sign_in(@user,no_capybara: true)
-                 expect do
-                      xhr :post, :create, :food=> @food.id
-                 end.to change(Basket, :count).by(1)
-                 expect do
-                      xhr :post, :create, :food=> @food.id
-                 end.not_to change(Basket, :count)
+      @user=FactoryGirl.create(:user)
+      @food=FactoryGirl.create(:food)
+      sign_in(@user, no_capybara: true)
+      expect do
+        xhr :post, :create, :food => @food.id
+      end.to change(Basket, :count).by(1)
+      expect do
+        xhr :post, :create, :food => @food.id
+      end.not_to change(Basket, :count)
 
-            end
-          it "destroy basket" do
-               @user=FactoryGirl.create(:user)
-               @food=FactoryGirl.create(:food)
-               sign_in(@user,no_capybara: true)
-               @basket=Basket.create(food_id:@food.id,user_id:@user.id,food_course:@food.course)
-               expect do
-                     xhr :delete, :destroy, :id => @basket.id
-                     end.to change(Basket, :count).by(-1)
-               User.delete_all
-          end
+    end
+    it "destroy basket" do
+      @user=FactoryGirl.create(:user)
+      @food=FactoryGirl.create(:food)
+      sign_in(@user, no_capybara: true)
+      @basket=Basket.create(food_id: @food.id, user_id: @user.id, food_course: @food.course)
+      expect do
+        xhr :delete, :destroy, :id => @basket.id
+      end.to change(Basket, :count).by(-1)
+      User.delete_all
+    end
 
-       end
+  end
 end
