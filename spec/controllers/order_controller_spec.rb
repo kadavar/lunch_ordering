80d155ba480083feb11383975c7c017501009ad9
Alexandr <kadavar32@yahoo.com>
2015-10-03 05:14:39 +0300

    json spec

--- spec/controllers/order_controller_spec.rb
+++ spec/controllers/order_controller_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe OrdersController, :type => :controller do
 
     it "displays all users" do
       @user=FactoryGirl.create(:user)
-      @order=Order.create(user_id:@user.id)
+      sign_in(@user,no_capybara: true)
       get :getJsons, :format => :json
-      @order_json = [{"order" => {"id" => @order.id, "user_id" => @user.id, "created_at" => Order.last.created_at, "updated_at" => Order.last.updated_at}}].to_json
-      expect(Order.getJson).to  be_json_eql(@order_json)
+
+       response.should be_success
 
     end
   end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,12 +1,12 @@
 include ApplicationHelper
-
+@current_user=User.none
 def sign_in(user, options={})
   if options[:no_capybara]
     # Sign in when not using Capybara.
     remember_token = User.new_remember_token
     cookies[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
-
+    @current_user = user
   else
     visit signin_path
     fill_in "Email", with: user.email
@@ -15,7 +15,14 @@ def sign_in(user, options={})
 
   end
 end
+def current_user=(user)
+  @current_user = user
+end
 
+def current_user
+  remember_token = User.encrypt(cookies[:remember_token])
+  @current_user ||= User.find_by(remember_token: remember_token)
+end
   def create_admin(user)
     Role.create!(role: "admin")
     Role.create!(role: "user")
